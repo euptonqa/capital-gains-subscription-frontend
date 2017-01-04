2b1735ad5a1e6fea5e46645c2e18d6a8ce419a67
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-01-04 15:39:20 +0000

    Fix the inevitable compilation errors

--- app/controllers/HelloWorld.scala
+++ app/controllers/HelloWorld.scala
@@ -19,7 +19,8 @@ package controllers
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import play.api.mvc._
 import scala.concurrent.Future
-
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
 
 object HelloWorld extends HelloWorld
 
