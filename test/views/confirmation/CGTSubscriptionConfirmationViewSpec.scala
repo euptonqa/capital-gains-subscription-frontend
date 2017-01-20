dfa89be5276336e5bbd60e6e63d945e12c0efa5c
DavidWoodward42 <david.woodward@digital.hmrc.gov.uk>
2017-01-20 09:53:01 +0000

    Adding the changes to fix merge conflicts.

---
+++ test/views/confirmation/CGTSubscriptionConfirmationViewSpec.scala
@@ -0,0 +1,11 @@
+
+
+package views.confirmation
+
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+class CGTSubscriptionConfirmationViewSpec extends UnitSpec with WithFakeApplication {
+
+
+
+}
